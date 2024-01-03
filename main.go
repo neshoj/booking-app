5467ddbd7225576dfbb024e38ae52600338c0b90
neshoj <ignatiusojiambo@gmail.com>
2024-01-03 09:14:00 +0300

    Feat - Included const variable types - Includes formatting of output with fmt.printf

--- main.go
+++ main.go
@@ -3,5 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Print("Hello World")
+	var conferenceName = "Go Conference of 2024"
+	const availableTickets = 50
+	var remainingTicket = 46
+
+	fmt.Println("Welcome to " + conferenceName)
+	fmt.Println("No of tickets remaining ", remainingTicket)
+	fmt.Printf("We are being booked quickly %v/%v tickets available", remainingTicket, availableTickets)
 }
