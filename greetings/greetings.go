c4caa4d0005574e2a2065a5ecfab5f12b9f1e920
neshoj <ignatiusojiambo@gmail.com>
2024-06-01 15:53:02 +0300

    Feat - added exception through - added error handling

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,9 +1,32 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"math/rand"
+)
 
-func Hello(name string) string {
+func Hello(name string) (string, error) {
 
-	message := fmt.Sprintf("Hi %v. Welcome! ", name)
-	return message
+	if name == "" {
+		return "", errors.New("Missing name")
+	}
+
+	message := fmt.Sprintf(randomGreetings(), name)
+	return message, nil
+}
+
+func Hellos(names []string) (map[string]string, error) {
+	messages := make(map[string]string)
+}
+
+func randomGreetings() string {
+
+	formats := []string{
+		"Hi, %v. Welcome!",
+		"Great to see you, %v!",
+		"Hail, %v! Well met!",
+	}
+
+	return formats[rand.Intn(len(formats))]
 }
--- hello/main.go
+++ hello/main.go
@@ -3,18 +3,21 @@ package main
 import (
 	"fmt"
 	"greetings/greetings"
+	"log"
 	"rsc.io/quote"
 )
 
 func main() {
-	var conferenceName = "Go Conference of 2024"
-	const availableTickets = 50
-	var remainingTicket = 46
-
-	fmt.Println("Welcome to " + conferenceName)
-	fmt.Println("No of tickets remaining ", remainingTicket)
-	fmt.Printf("We are being booked quickly %v/%v tickets available", remainingTicket, availableTickets)
+	log.SetPrefix("Greetings: ")
+	log.SetFlags(0)
 
 	fmt.Println(quote.Go())
-	fmt.Println(greetings.Hello("John"))
+	message, err := greetings.Hello("John")
+
+	if err != nil {
+
+		log.Fatalf("Unable to print greeting %v", err)
+	}
+
+	fmt.Println(message)
 }
